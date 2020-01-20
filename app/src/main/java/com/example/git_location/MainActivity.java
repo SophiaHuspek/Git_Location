bfd8b538b2b002e3aaa19cb6aa1c22e15a1a61f5
Sophia Huspek <sophia.huspek@gmail.com>
2020-01-20 15:59:02 +0100

    blabla

--- app/src/main/java/com/example/git_location/MainActivity.java
+++ app/src/main/java/com/example/git_location/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.git_location;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    private  static LocationManager locMan = null;
+    private static LocationManager locMan = null;
 
     LinkedList<MyLocation> locations = new LinkedList();
 
@@ -26,5 +31,60 @@ public class MainActivity extends AppCompatActivity {
     public void btnAnzeigenClicked(View view) {
 
 
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        if(loc == null) return;
+        displayLocation(loc);
+
+    }
+
+    private void displayLocation(Location loc)
+    {
+        String latitude = loc.getLatitude() + "";
+        String longitude = loc.getLongitude() +"";
+
+        //MyLocation myloc = new MyLocation(longitude, latitude, )
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        locMan.removeUpdates(this);
     }
 }
