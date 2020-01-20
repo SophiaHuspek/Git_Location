db9c356fcaca6370967c2a1874a07a014f73f14f
Lea Litzlbauer <lea.litzlbauer@gmail.com>
2020-01-20 15:37:15 +0100

    commit_lea

--- app/src/main/java/com/example/git_location/MainActivity.java
+++ app/src/main/java/com/example/git_location/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.git_location;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,12 +10,16 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  static LocationManager locMan = null;
+
     LinkedList<MyLocation> locations = new LinkedList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
 
     }
 
