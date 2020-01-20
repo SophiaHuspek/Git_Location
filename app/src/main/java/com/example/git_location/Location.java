db8091d288c2f5ab43a121f776887d854bc14401
Sophia Huspek <sophia.huspek@gmail.com>
2020-01-20 15:30:30 +0100

    Klasse Location + onClickEvent fuer Button Anzeigen

---
+++ app/src/main/java/com/example/git_location/Location.java
@@ -0,0 +1,51 @@
+package com.example.git_location;
+
+public class Location {
+
+    String longitude;
+    String latitude;
+    String date;
+    String time;
+
+    public Location() {
+    }
+
+    public Location(String longitude, String latitude, String date, String time) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
--- app/src/main/java/com/example/git_location/MainActivity.java
+++ app/src/main/java/com/example/git_location/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.git_location;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinkedList<>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnAnzeigenClicked(View view) {
+
 
     }
 }
